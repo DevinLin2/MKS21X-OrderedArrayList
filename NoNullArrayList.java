db1a479e076a9be623858b972c863535ded6ee19
VitaminOh <dlin01@stuy.edu>
2018-11-01 11:01:23 -0400

    should be finished

---
+++ NoNullArrayList.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+  public NoNullArrayList(int initialCapacity){
+    super(initialCapacity);
+  }
+  public boolean add(T value){
+    if (value == null){
+      throw new IllegalArgumentException("Value null cannot be added to ArrayList");
+    }else{
+      super.add(value);
+    }
+  }
+  public void add(int index, T value){
+    if (value == null){
+      throw new IllegalArgumentException("Value null cannot be added to ArrayList");
+    }else{
+      super.add(index, value);
+    }
+  }
+  public T set(int index, T value){
+    if (value == null){
+      throw new IllegalArgumentException("Value null cannot be set to ArrayList");
+    }else{
+      super.set(index, value);
+    }
+  }
+}
